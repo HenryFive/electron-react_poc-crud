e032e93b91f0369b4a9e2aa59f5b2e7d9ccb7eed
Henry James Cinco <henryjamescinco@yahoo.com>
2021-09-21 09:45:01 +0800

    fearture/interface

--- src/App.js
+++ src/App.js
@@ -1,24 +1,273 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import { DragDropContext, Droppable } from "react-beautiful-dnd";
+import ToDoListComponent from "./Components/toDoListComponent";
+import { Helmet } from "react-helmet";
+const reorder = (list, startIndex, endIndex) => {
+  const [removed] = list.splice(startIndex, 1);
+  list.splice(endIndex, 0, removed);
+  return list;
+};
+let max = 0;
 function App() {
+  let tasks = [
+    { id: "0", content: "first", isCompleted: true },
+    { id: "1", content: "second", isCompleted: false },
+    { id: "2", content: "third", isCompleted: false }
+  ];
+
+  function onDragEnd(result) {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
+    }
+
+    const files = reorder(
+      allFilters,
+      result.source.index,
+      result.destination.index
+    );
+
+    setAll(files);
+  }
+
+  const [darkmode, setDark] = useState(false);
+  const [inputVal, setValue] = useState("");
+  const [allFilters, setAll] = useState(tasks);
+  const [doingFilters, setDoing] = useState(
+    allFilters.filter((el) => el.isCompleted === false)
+  );
+  const [completeFilters, setComplete] = useState(
+    allFilters.filter((el) => el.isCompleted === true)
+  );
+  const [allActive, switchAllBool] = useState(true);
+  const [completeActive, switchCompleteBool] = useState(false);
+  const [doingActive, switchDoingBool] = useState(false);
+  const addNewTask = (e) => {
+    max += 1;
+    e.preventDefault();
+    allFilters.push({
+      content: inputVal,
+      isCompleted: false,
+      id: `${max}`
+    });
+    console.log(allFilters[allFilters.length - 1]);
+    refresh();
+    setValue("");
+  };
+
+  const handleDark = function () {
+    setDark(!darkmode);
+  };
+
+  const handleChange = function (e) {
+    setValue(e.target.value);
+  };
+  const switchComplete = () => {
+    switchAllBool(false);
+    switchCompleteBool(true);
+    switchDoingBool(false);
+    refresh();
+  };
+
+  const switchDoing = () => {
+    switchAllBool(false);
+    switchCompleteBool(false);
+    switchDoingBool(true);
+    refresh();
+  };
+
+  const switchAll = () => {
+    switchAllBool(true);
+    switchCompleteBool(false);
+    switchDoingBool(false);
+    setAll(allFilters);
+    refresh();
+  };
+
+  const handleDelete = (target) => {
+    setAll(allFilters.filter((el) => el !== target));
+    setComplete(
+      allFilters.filter((el) => el.isCompleted === true && el !== target)
+    );
+    setDoing(
+      allFilters.filter((el) => el.isCompleted === false && el !== target)
+    );
+  };
+
+  const clearCompleted = () => {
+    setAll(allFilters.filter((el) => el.isCompleted === false));
+    setComplete([]);
+  };
+  const refresh = function () {
+    setAll(allFilters);
+    setComplete(allFilters.filter((el) => el.isCompleted === true));
+    setDoing(allFilters.filter((el) => el.isCompleted === false));
+  };
+  console.log(allFilters);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <Helmet>
+        <style>{` ${
+          darkmode && "body { background-color: #25273c; }"
+        }yarn add react-helmet`}</style>
+      </Helmet>
+      <div className={"flex flex-jc-c flex-ai-c"}>
+        <img
+          src={
+            darkmode
+              ? "/images/bg-desktop-dark.jpg"
+              : "/images/bg-desktop-light.jpg"
+          }
+          alt="light-img"
+          className="canvas hide-for-mobile"
+        />
+        <img
+          src={
+            darkmode
+              ? "/images/bg-mobile-dark.jpg"
+              : "/images/bg-mobile-light.jpg"
+          }
+          alt="light-img"
+          className="canvas canvas-mobile hide-for-desktop"
+        />
+        <div className="wrapper">
+          <div className="header flex flex-jc-sb flex-ai-c">
+            <h1 style={{ color: "#fff" }}>T O D O</h1>
+            <img
+              src="/images/icon-moon.svg"
+              alt="moon"
+              style={{ height: "1.875em", cursor: "pointer" }}
+              onClick={handleDark}
+            />
+          </div>
+          <div
+            className={`create flex flex-ai-c ${darkmode && "darkModeColor"}`}
+          >
+            <div className="check"></div>
+            <form onSubmit={addNewTask}>
+              <input
+                type="text"
+                className="create-textbox"
+                placeholder="Create a new todo..."
+                value={inputVal}
+                onChange={handleChange}
+              />
+            </form>
+          </div>
+
+          <ul
+            style={{
+              // height: `${4 * allFilters.length}em`,
+              backgroundColor: `${darkmode ? "#484b6a" : "#fff"}`,
+              height: "fit-content"
+            }}
+          >
+            <Droppable droppableId={"0"}>
+              {(provided) => (
+                <div ref={provided.innerRef} {...provided.droppableProps}>
+                  {allActive &&
+                    allFilters.map((el, index) => (
+                      <ToDoListComponent
+                        task={el}
+                        id={el.id}
+                        index={index}
+                        handleDelete={handleDelete}
+                        refresh={refresh}
+                        darkmode={darkmode}
+                      ></ToDoListComponent>
+                    ))}
+                  {doingActive &&
+                    doingFilters.map((el, index) => (
+                      <ToDoListComponent
+                        task={el}
+                        id={el.id}
+                        index={index}
+                        handleDelete={handleDelete}
+                        refresh={refresh}
+                        darkmode={darkmode}
+                      ></ToDoListComponent>
+                    ))}
+                  {completeActive &&
+                    completeFilters.map((el, index) => (
+                      <ToDoListComponent
+                        task={el}
+                        id={el.id}
+                        index={index}
+                        handleDelete={handleDelete}
+                        refresh={refresh}
+                        darkmode={darkmode}
+                      ></ToDoListComponent>
+                    ))}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          </ul>
+
+          <div
+            className={`options flex flex-jc-sb flex-ai-c ${
+              darkmode && "darkModeColor"
+            }`}
+            style={{ color: "#777a92" }}
+          >
+            <div className="task-left">
+              {allFilters.filter((el) => el.isCompleted === false).length} items
+              left
+            </div>
+            <div className="filter flex hide-for-mobile">
+              <div
+                className={`all ${allActive && "selected"}`}
+                onClick={switchAll}
+              >
+                All
+              </div>
+              <div
+                className={`active ${doingActive && "selected"}`}
+                onClick={switchDoing}
+              >
+                Active
+              </div>
+              <div
+                className={`complete ${completeActive && "selected"}`}
+                onClick={switchComplete}
+              >
+                Completed
+              </div>
+            </div>
+            <div className="clear-completed" onClick={clearCompleted}>
+              Clear Completed
+            </div>
+          </div>
+
+          <div
+            className={`filter flex flex-ai-c flex-jc-c hide-for-desktop ${
+              darkmode && "darkModeColor"
+            }`}
+          >
+            <div
+              className={`all ${allActive && "selected"}`}
+              onClick={switchAll}
+            >
+              All
+            </div>
+            <div
+              className={`active ${doingActive && "selected"}`}
+              onClick={switchDoing}
+            >
+              Active
+            </div>
+            <div
+              className={`complete ${completeActive && "selected"}`}
+              onClick={switchComplete}
+            >
+              Completed
+            </div>
+          </div>
+          <div className="help">Drag and Drop to reorder list</div>
+        </div>
+      </div>
+    </DragDropContext>
   );
 }
 
