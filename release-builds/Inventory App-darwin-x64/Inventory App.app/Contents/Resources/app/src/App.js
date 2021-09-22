09c4fdd08f8784abc7a1dc188e851460c13d32d5
Henry James Cinco <henryjamescinco@yahoo.com>
2021-09-22 17:32:20 +0800

    build successful

---
+++ release-builds/Inventory App-darwin-x64/Inventory App.app/Contents/Resources/app/src/App.js
@@ -0,0 +1,73 @@
+import React, { useState } from "react";
+import InventoryTable from "./Components/Tables/inventoryTable";
+import AddItem from "./Components/Forms/addItem";
+import EditItem from "./Components/Forms/editItem";
+import "./App.css";
+
+const App = () => {
+  const itemsData = [
+    { id: 1, name: "Tray", description: "Aluminum from Pakistan", qty: 10 },
+    { id: 2, name: "Couldron", description: "Copper, small", qty: 3 },
+    { id: 3, name: "Spoons", description: "Stainless steel", qty: 22 },
+    { id: 4, name: "Forks", description: "Stainless steel", qty: 22 }
+  ];
+
+  const [items, setItems] = useState(itemsData);
+
+  const addItem = item => {
+    item.id = items.length + 1;
+    setItems([...items, item]);
+  };
+
+  const deleteItem = id => {
+    setEditing(false);
+    setItems(items.filter(item => item.id !== id));
+  };
+
+  const [editing, setEditing] = useState(false);
+  const initialFormState = { id: null, name: "", description: "", qty: 12  };
+  const [currentItem, setCurrentItem] = useState(initialFormState);
+
+  const editRow = item => {
+    setEditing(true);
+    setCurrentItem({ id: item.id, name: item.name, description: item.description, qty: item.qty });
+  };
+
+  const updateItem = (id, updatedItem) => {
+    setEditing(false);
+
+    setItems(items.map(item => (item.id === id ? updatedItem : item)));
+  };
+
+  return (
+    <section className="main">
+      <h1>Inventory</h1>
+      <section>
+        <section>
+          <h2>View items</h2>
+          <InventoryTable items={items} editRow={editRow} deleteItem={deleteItem} />
+        </section>
+        <section>
+          {editing ? ( 
+            <div>
+              <h2>Edit item</h2>
+              <EditItem
+                editing={editing}
+                setEditing={setEditing}
+                currentItem={currentItem}
+                updateItem={updateItem}
+              />
+            </div>
+          ) : (
+            <div>
+              <h2>Add item</h2>
+              <AddItem addItem={addItem} />
+            </div>
+          )}
+        </section>
+      </section>
+    </section>
+  );
+};
+
+export default App;
---
+++ release-builds/Inventory App-darwin-x64/Inventory App.app/Contents/Resources/app/src/Components/Tables/inventoryTable.js
@@ -0,0 +1,47 @@
+import React from "react";
+import "./inventoryTable.css"
+
+const InventoryTable = props => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Description</th>
+          <th>Qty</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.items.length > 0 ? (
+          props.items.map(item => (
+            <tr key={item.id}>
+              <td>{item.name}</td>
+              <td>{item.description}</td>
+              <td>{item.qty}</td>
+              <td className="table--actions">
+                <button
+                  onClick={() => props.editRow(item)}
+                >
+                  Edit
+                </button>
+                <button
+                  onClick={() => props.deleteItem(item.id)}
+                  className="table__delete-button"
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))
+        ) : (
+          <tr>
+            <td colSpan={10}>No items</td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  );
+};
+
+export default InventoryTable;
